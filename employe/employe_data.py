44d454faae62d3a36b54a436d0e160d6b642fe2e
sudhakar <sudhaswain07@gmail.com>
2023-01-05 11:00:33 +0530

    second commit

--- employe/employe_data.py
+++ employe/employe_data.py
@@ -40,5 +40,6 @@ employee ='{"id":"09", "name": "Nitin", "department":"Finance"}'
 # Convert string to Python dict
 employee_dict = json.loads(employee)
 print(employee_dict)
-
+data = "helo sudhakar "
+print(data)
 
